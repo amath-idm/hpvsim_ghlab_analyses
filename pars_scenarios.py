67a9048c36bbb1c32818d276924678fd2c83bda0
Jamie Cohen <jamie.alex.cohen@gmail.com>
2022-11-30 09:11:01 -0800

    typo fix!

--- pars_scenarios.py
+++ pars_scenarios.py
@@ -16,7 +16,7 @@ def get_screen_intvs(location, screen_scen=None, screen_prod=None,
     # Create inputs
     if isinstance(screen_prod, list):
         primary_screen = screen_prod[0] # Primary screening product
-        triage_screen = screen_prod[1]
+        triage_screen = screen_prod[1][0]
     else:
         primary_screen = screen_prod
         triage_screen = None
