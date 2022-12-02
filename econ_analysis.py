0ca35204962ab054a0cae37a05721c9130842c94
Jamie Cohen <jamie.alex.cohen@gmail.com>
2022-12-01 20:19:55 -0800

    cleanup, discounting (yay!)

--- econ_analysis.py
+++ econ_analysis.py
@@ -12,6 +12,7 @@ import pandas as pd
 from scipy.stats import truncnorm
 import sciris as sc
 import pylab as pl
+import seaborn as sns
 
 resfolder = 'results'
 figfolder = 'figures'
@@ -57,6 +58,12 @@ cost_params['HPV'] = np.array([14.8, 36, 9.1])
 cost_params['HPV_sd'] = (2*1.96)*3.8/14.8
 cost_params['VIA'] = np.array([5.2, 13, 2.89])
 cost_params['VIA_sd'] = (2*1.96)*1.3/5.2
+
+cost_params['POC_HPV'] = np.array([3, 5, 4])
+cost_params['POC_HPV_sd'] = (2*1.96)*3.8/14.8
+cost_params['AVE'] = np.array([5.2, 5, 2.89])
+cost_params['AVE_sd'] = (2*1.96)*1.3/5.2
+
 cost_params['CIN'] = np.array([60, 3.5, 3.57])
 cost_params['CIN_sd'] = (2*1.96)*4.2/16
 cost_params['cancer'] = np.array([450, np.mean(np.array([44.73, 64.13, 281.5, 768, 212])), np.mean(np.array([94, 574, 974, 21]))])
@@ -100,9 +107,15 @@ for location in locations:
     simulated_costs['HPV'] = truncnorm.rvs((lower_clip - costs['HPV']) / costs['HPV_sd'],
                                            (upper_clip - costs['HPV']) / costs['HPV_sd'],
                                            loc=costs['HPV'], scale=costs['HPV_sd'], size=n_seeds)
+    simulated_costs['POC_HPV'] = truncnorm.rvs((lower_clip - costs['POC_HPV']) / costs['POC_HPV_sd'],
+                                           (upper_clip - costs['POC_HPV']) / costs['POC_HPV_sd'],
+                                           loc=costs['HPV'], scale=costs['HPV_sd'], size=n_seeds)
     simulated_costs['VIA'] = truncnorm.rvs((lower_clip - costs['VIA']) / costs['VIA_sd'],
                                            (upper_clip - costs['VIA']) / costs['VIA_sd'],
                                            loc=costs['VIA'], scale=costs['VIA_sd'], size=n_seeds)
+    simulated_costs['AVE'] = truncnorm.rvs((lower_clip - costs['AVE']) / costs['AVE_sd'],
+                                           (upper_clip - costs['AVE']) / costs['AVE_sd'],
+                                           loc=costs['AVE'], scale=costs['AVE_sd'], size=n_seeds)
     simulated_costs['CIN'] = truncnorm.rvs((lower_clip - costs['CIN']) / costs['CIN_sd'],
                                            (upper_clip - costs['CIN']) / costs['CIN_sd'],
                                            loc=costs['CIN'], scale=costs['CIN_sd'], size=n_seeds)
@@ -123,22 +136,31 @@ for location in locations:
     for scenario in scenarios:
         df = pd.DataFrame()
         model_output = model_res[(model_res['location'] == location) & (model_res['scen_label'] == scenario)]
-
         ylls = []
         ylds = []
         dalys = []
+        total_costs = []
         for name, group in model_output.groupby('seed'):
+            discounted_cancers = np.array([i/1.03**t for t,i in enumerate(group['new_cancers'].values)])
+            discounted_cancer_deaths = np.array([i/1.03**t for t,i in enumerate(group['new_cancer_deaths'].values)])
             avg_age_ca_death = np.mean(group['av_age_cancer_deaths'])
             avg_age_ca = np.mean(group['av_age_cancers'])
             ca_years = avg_age_ca_death - avg_age_ca
-            yld = np.sum(np.mean([0.54, 0.049, 0.451, 0.288]) * ca_years * group['new_cancers'].values)
+            yld = np.sum(np.mean([0.54, 0.049, 0.451, 0.288]) * ca_years * discounted_cancers)
             ylds += [yld]
             ind = sc.findnearest(life_expectancy['AgeGrpStart'], avg_age_ca_death)
-            yll = np.sum(life_expectancy['ex'][ind] * group['new_cancer_deaths'].values)
+            yll = np.sum(life_expectancy['ex'][ind] * discounted_cancer_deaths)
             ylls += [yll]
             daly = yll + yld
             dalys += [daly]
-            # daly_averted =
+            total_cost = (group['new_hpv_screens'].values * costs['HPV'].values[name]) + \
+                            (group['new_via_screens'].values * costs['VIA'].values[name]) + \
+                            (group['new_poc_hpv_screens'].values * costs['POC_HPV'].values[name]) + \
+                            (group['new_ave_screens'].values * costs['AVE'].values[name]) + \
+                            ((group['new_thermal_ablations'].values + group['new_leeps'].values) * costs['CIN'].values[name]) + \
+                            (group['new_cancer_treatments'].values * costs['cancer'].values[name])
+            discounted_cost = np.sum([i/1.03**t for t,i in enumerate(total_cost)])
+            total_costs += [discounted_cost]
 
         df['ylls'] = ylls
         df['ylds'] = ylds
@@ -146,23 +168,7 @@ for location in locations:
         if scenario == 'No screening':
             base_DALYs = dalys
         df['DALYs_averted'] = base_DALYs - df['DALYs']
-
-        means = model_output.groupby('seed')[['new_cancers', 'new_cancer_deaths', 'new_hpv_screens',
-                                              'new_poc_hpv_screens', 'new_via_screens', 'new_ave_screens',
-                                              'new_thermal_ablations', 'new_leeps', 'new_cancer_treatments']].sum()
-        df['seed'] = means.index.values
-        df['total_cancers'] = means['new_cancers'].values
-        df['total_cancer_deaths'] = means['new_cancer_deaths'].values#.sum()]
-        df['total_hpv_screens'] = means['new_hpv_screens'].values#.sum()]
-        df['total_poc_hpv_screens'] = means['new_poc_hpv_screens'].values#.sum()]
-        df['total_via_screens'] = means['new_via_screens'].values#.sum()]
-        df['total_ave_screens'] = means['new_ave_screens'].values#.sum()]
-        df['total_cin_treatments'] = means['new_thermal_ablations'].values + means['new_leeps'].values#.sum()]#.sum()]
-        df['total_cancer_treatments'] = means['new_cancer_treatments'].values#.sum()]
-        df['total_costs'] = (df['total_hpv_screens'].values * costs['HPV'].values) + \
-                            (df['total_via_screens'].values * costs['VIA'].values) + \
-                            (df['total_cin_treatments'].values * costs['CIN'].values) + \
-                            (df['total_cancer_treatments'].values * costs['cancer'].values)
+        df['total_costs'] = total_costs
         df['location'] = location
         df['scen_label'] = scenario
         dfs += df
@@ -173,9 +179,12 @@ set_font(size=20)
 markers = ['.', 'v', '<', '1', 's', 'p', 'P', '*', '+', 'D', '^', 'x']
 colors = sc.gridcolors(len(scenarios))
 fig, ax = pl.subplots(figsize=(16, 10))
-grouped = alldfs.groupby('scen_label')
-for i, (key, group) in enumerate(grouped):
-    group.plot(ax=ax, kind='scatter', x='DALYs_averted', y='total_costs', label=key, marker=markers[i], s=100)
+grouped_means = alldfs.groupby('scen_label').mean()
+grouped_means['scen_label'] = grouped_means.index.values
+
+for i, scen in enumerate(scenarios):
+    group = grouped_means[grouped_means['scen_label'] == scen]
+    group.plot(ax=ax, kind='scatter', x='DALYs_averted', y='total_costs', label=scen, marker=markers[i], s=100)
 
 ax.set_xlabel('DALYs averted')
 ax.set_ylabel('Total costs')
