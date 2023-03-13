c9c1b423c07de87760c924a39c00780daa25e02a
Robyn Stuart <robyna.s@gmail.com>
2023-03-13 19:13:30 +1100

    add result file

--- version.py
+++ version.py
@@ -4,6 +4,6 @@ Version and license information.
 
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__ = '0.1.0'
-__versiondate__ = '2022-11-30'
-__license__ = f'HPVsim GHlabs collaboration {__version__} ({__versiondate__}) — © 2022 by IDM'
+__version__ = '0.1.1'
+__versiondate__ = '2023-03-13'
+__license__ = f'HPVsim GHlabs collaboration {__version__} ({__versiondate__}) — © 2023 by IDM'
